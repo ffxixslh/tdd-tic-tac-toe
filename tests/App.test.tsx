89e30c67cc798a996f8234f72c2a0719589d736d
luohao <1223048059@qq.com>
2024-07-02 01:33:22 +0800

    first commit

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,13 +1,14 @@
 import { it, describe, expect } from 'vitest'
 import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import App from '../src/App'
 import React from 'react'
 
 describe('App', () => {
   it('should render', () => {
-    const { getByText } = render(<App />)
-    const element = getByText('hello world')
-    expect(element).toBeDefined()
+    render(<App />)
+    const element = screen.getByText('hello world')
+    expect(element).toBeInTheDocument()
   })
 })
 
