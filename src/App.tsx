79f2ccb01cba1223cb48f2c90f565de2376e33e4
luohao <1223048059@qq.com>
2024-07-02 02:12:43 +0800

    chore: chore

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div data-testid='app' className='container'>
-      <div>
-        <div className='text-red-400'>Count: {count}</div>
-        <button onClick={() => setCount(current => current + 1)}>Increment</button>
-        <button onClick={() => setCount(current => current - 1)}>Decrement</button>
+    <div data-testid='app' id='app' className='container flex align-center justify-center'>
+      <div className='flex flex-col justify-center'>
+        <div className='self-center text-red-400'>Count: {count}</div>
+        <div className='flex gap-4'>
+          <button onClick={() => setCount(current => current + 1)}>Increment</button>
+          <button onClick={() => setCount(current => current - 1)}>Decrement</button>
+        </div>
       </div>
     </div>
   )
